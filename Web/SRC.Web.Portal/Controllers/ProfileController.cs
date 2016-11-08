772f64d3ced32f0cdec05867b54445ee5abcae11
burak.yilmaz <burak.yilmaz@etstur.com>
2016-11-08 19:11:43 +0300

    sad

---
+++ Web/SRC.Web.Portal/Controllers/ProfileController.cs
@@ -0,0 +1,39 @@
+﻿using SRC.Library.Entities.CrmEntities;
+using SRC.Web.Portal.MockData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SRC.Web.Portal.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public ProfileController()
+        {
+
+        }
+
+        public ActionResult Index(Contact model)
+        {
+            model = new Contact();
+            model = ContactMock.GetContact();
+            return View(model);
+        }
+
+        public ActionResult Edit(Contact model)
+        {
+            model = new Contact();
+            model = ContactMock.GetContact();
+            return View(model);
+        }
+
+        public ActionResult ChangePassword(Contact model)
+        {
+            model = new Contact();
+            model = ContactMock.GetContact();
+            return RedirectToAction("Edit");
+        }
+    }
+}
---
+++ Web/SRC.Web.Portal/MockData/ContactMock.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SRC.Library.Entities;
+using SRC.Library.Entities.CrmEntities;
+
+namespace SRC.Web.Portal.MockData
+{
+    public static class ContactMock
+    {
+        public static Contact GetContact()
+        {
+            Contact contact  = new Contact();
+            contact.Id = Guid.NewGuid();
+            contact.FirstName = "Burak";
+            contact.LastName = "Yılmaz";
+            contact.BirthDate = DateTime.Now.AddYears(-20);
+            contact.Gender = new OptionSetValueWrapper()
+            {
+                AttributeValue = 1,
+                Value = "Erkek"
+            };
+            contact.MobilePhone = "05438499258";
+            contact.GsmOperator = new EntityReferenceWrapper()
+            {
+                Id = Guid.NewGuid(),
+                LogicalName = "new_gsmoperator",
+                Name = "Turkcell"
+            };
+            contact.WorkPhone = "02122653214";
+            contact.Fax = "02122653215";
+            contact.EmailAddress = "burak.yilmaz@immib.org.tr";
+            contact.TaxNumber = "123456798";
+            contact.CompanyName = "SRC Danışmanlık";
+            contact.CompanyPosition = "CTO";
+            contact.TaxOffice = new EntityReferenceWrapper()
+            {
+                Id = Guid.NewGuid(),
+                LogicalName = "new_taxxoffice",
+                Name = "Beyoğlu"
+            };
+            contact.CompanyAddress = "Bağdat Caddesi No:24 Kadıköy/İstanbul";
+            contact.InformedBy = new EntityReferenceWrapper()
+            {
+                Id = Guid.NewGuid(),
+                LogicalName = "new_informedby",
+                Name = "Sosyal Medya"
+            };
+            contact.City = new EntityReferenceWrapper()
+            {
+                Id = Guid.NewGuid(),
+                LogicalName = "new_informedby",
+                Name = "Sosyal Medya"
+            };
+            contact.EducationLevel = new OptionSetValueWrapper()
+            {
+                AttributeValue = (int)Contact.EducationLevelCode.HIGH_SCHOOL,
+                Value = "Lise"
+            };
+
+            return contact;
+        }
+    }
+}
