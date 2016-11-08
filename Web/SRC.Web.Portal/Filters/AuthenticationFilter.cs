de8a0a5174ec83bf717e55585f8b28cc583ad407
Volkan SERTER <volkan.serter@sahibinden.com>
2016-11-08 19:05:12 +0300

    commit

---
+++ Web/SRC.Web.Portal/Filters/AuthenticationFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SRC.Web.Portal.Filters
+{
+    public class AuthenticationFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            //TODO: Session Check
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
