282ae8fdd33f4a3c942ab70cfb4eac892ae6f266
Volkan SERTER <volkan.serter@sahibinden.com>
2017-02-25 15:52:55 +0300

    snk files added

---
+++ Web/SRC.Web.Portal/Filters/ErrorAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SRC.Web.Portal.Filters
+{
+    public class ErrorAttribute : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            Exception ex = filterContext.Exception;
+            filterContext.ExceptionHandled = true;
+            var model = new HandleErrorInfo(filterContext.Exception, "Error", "Index");
+
+            filterContext.Result = new ViewResult()
+            {
+                ViewName = "Error",
+                ViewData = new ViewDataDictionary(model)
+            };
+        }
+    }
+}
---
+++ WindowsService/SRC.WindowsService.Test/ISmsManager.cs
@@ -0,0 +1,10 @@
+﻿using SRC.Library.Entities.CrmEntities;
+using System;
+namespace SRC.WindowsService.TestService.Interfaces
+{
+    public interface ISmsManager
+    {
+        string GetSession();
+        string SendSms(SmsEnt smsEntity, string sessionId);
+    }
+}
