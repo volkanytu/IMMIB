5d5c0041faf14c0f3f1dbb7838c0bec72a115563
Burak Yılmaz <kemalburakyilmaz@hotmail.com>
2017-03-12 19:15:42 +0300

    Plugin

---
+++ Plugins/SRC.Plugins.CrmPlugin/EducationAttendancePlugin/EducationAttendancePreCreate.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xrm.Sdk;
+using SRC.Plugins.CrmPlugin.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRC.Plugins.CrmPlugin.EducationAttendancePlugin
+{
+    public class EducatioAttendancenPreCreate : BasePlugin, IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            base.DoWork(serviceProvider, TaskType.EDUCATION_ATTENDANCE, PluginOperation.PRE_CREATE);
+        }
+    }
+}
