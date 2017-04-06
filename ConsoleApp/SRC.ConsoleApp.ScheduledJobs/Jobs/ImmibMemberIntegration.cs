b565258865fcc811bd548b37e18c206ca16d6cc6
Burak Yılmaz <kemalburakyilmaz@hotmail.com>
2017-04-07 00:22:05 +0300

    İmmib member integration

---
+++ ConsoleApp/SRC.ConsoleApp.ScheduledJobs/Jobs/ImmibMemberIntegration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SRC.Library.Business.Interfaces;
+using SRC.Library.Services.Members.MemberService;
+using SRC.Library.Data.SqlDao;
+using SRC.Library.Domain.Business.Interfaces;
+using SRC.Library.Entities.CrmEntities;
+using SRC.Library.Entities.CustomEntities;
+using SRC.Library.LogManager.Interfaces;
+using SRC.Library.Services.Members;
+
+namespace SRC.ConsoleApp.ScheduledJobs.Jobs
+{
+    public class ImmibMemberIntegration: BaseJob
+    {
+        private ImmibMembers _service;
+        private ILogManager _logmanager;
+        private IAccountBusiness _accountBusiness;
+        private IBaseBusiness<Account> _baseBusiness;
+
+        public ImmibMemberIntegration(ILogManager logmanager, IBaseBusiness<Account> baseBusiness, IAccountBusiness accountBusiness)
+        {
+            _service = new ImmibMembers(); //Bunu ioc'ye taşımalı mıyız?
+            _accountBusiness = accountBusiness;
+            _baseBusiness = baseBusiness;
+            _logmanager = logmanager;
+        }
+        protected override void DoWork(string[] args)
+        {
+            List<ImmibMember> members = _service.GetMembers();
+
+            foreach (var immibMember in members)
+            {
+                Account account = _accountBusiness.GetAccount(immibMember.TCVERNO);
+                if (account == null)
+                {
+                    //immibmember to account
+                    _baseBusiness.Insert(account);
+                }
+            }
+        }
+    }
+}
---
+++ Library/SRC.Library.Entities/CustomEntities/ImmibMember.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRC.Library.Entities.CustomEntities
+{
+    public class ImmibMember
+    {
+        public decimal? BIRLIKKOD { get; set; }
+        public string BIRLIK { get; set; }
+        public DateTime GIRIS_TARIHI { get; set; }
+        public DateTime CIKIS_TARIHI { get; set; }
+        public string SICIL_NO { get; set; }
+        public string TCVERNO { get; set; }
+    }
+}
