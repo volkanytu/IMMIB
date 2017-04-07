f7d087a0796d266e53e7515e3978888fffd2222b
Volkan SERTER <volkan.serter@sahibinden.com>
2017-04-07 07:58:05 +0300

    Kredi kartı işlemi eklendi.

--- Web/SRC.Web.NewPortal/Controllers/EducationApiController.cs
+++ Web/SRC.Web.NewPortal/Controllers/EducationApiController.cs
@@ -16,6 +16,9 @@ using System.Web.Http;
 using SRC.Library.Entities;
 using System.Configuration;
 using SRC.Library.Common;
+using System.Xml;
+using System.Text;
+using System.IO;
 
 namespace SRC.Web.NewPortal.Controllers
 {
@@ -268,6 +271,8 @@ namespace SRC.Web.NewPortal.Controllers
             }
             else
             {
+                PreparePaymentXml(creditCardData);
+
                 if (creditCardData.AttendanceId != null)
                 {
                     creditCardData.EducationAttendance = creditCardData.AttendanceId.Value.ToEntityReferenceWrapper<EducationAttendance>();
@@ -378,6 +383,149 @@ namespace SRC.Web.NewPortal.Controllers
 
                 returnValue.Message = "Üye eğitim katılım özeti çekildi.";
             }
+
+            return returnValue;
+        }
+
+        private ResponseContainer<CreditCardResult> PreparePaymentXml(CreditCardLog creditCardData)
+        {
+            ResponseContainer<CreditCardResult> returnValue = new ResponseContainer<CreditCardResult>
+            {
+                Result = new CreditCardResult()
+            };
+
+            var paymentXmlResult = GetPaymentXmlText(creditCardData);
+
+            string xmlMessage = string.Empty;
+
+            if (paymentXmlResult.Success)
+            {
+                xmlMessage = paymentXmlResult.Result;
+            }
+
+            //oluşturduğumuz xml i vposa gönderiyoruz.
+            byte[] dataStream = Encoding.UTF8.GetBytes("prmstr=" + xmlMessage);
+            HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create("https://onlineodemetest.vakifbank.com.tr:4443/VposService/v3/Vposreq.aspx");//Vpos adresi
+            webRequest.Method = "POST";
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+            webRequest.ContentLength = dataStream.Length;
+            webRequest.KeepAlive = false;
+            string responseFromServer = "";
+
+            using (Stream newStream = webRequest.GetRequestStream())
+            {
+                newStream.Write(dataStream, 0, dataStream.Length);
+                newStream.Close();
+            }
+
+            using (WebResponse webResponse = webRequest.GetResponse())
+            {
+                using (StreamReader reader = new StreamReader(webResponse.GetResponseStream()))
+                {
+                    responseFromServer = reader.ReadToEnd();
+                    reader.Close();
+                }
+
+                webResponse.Close();
+            }
+
+            if (string.IsNullOrEmpty(responseFromServer))
+            {
+                returnValue.Message = "Kredi kartı işlem talebi boş döndü.";
+                return returnValue;
+            }
+
+            var xmlResponse = new XmlDocument();
+            xmlResponse.LoadXml(responseFromServer);
+            var resultCodeNode = xmlResponse.SelectSingleNode("VposResponse/ResultCode");
+            var resultDescriptionNode = xmlResponse.SelectSingleNode("VposResponse/ResultDetail");
+
+            if (resultCodeNode != null)
+            {
+                returnValue.Result.Code = resultCodeNode.InnerText;
+            }
+            if (resultDescriptionNode != null)
+            {
+                returnValue.Result.Description = resultDescriptionNode.InnerText;
+            }
+
+            returnValue.Message = string.Format("İşlem sonuç Kodu: {0}, Mesaj: {1}" + returnValue.Result.Code, returnValue.Result.Description);
+            returnValue.Success = true;
+
+            return returnValue;
+        }
+
+        private ResponseContainer<string> GetPaymentXmlText(CreditCardLog creditCardData)
+        {
+            ResponseContainer<string> returnValue = new ResponseContainer<string>();
+
+            XmlDocument xmlDoc = new XmlDocument();
+
+            XmlDeclaration xmlDeclaration = xmlDoc.CreateXmlDeclaration("1.0", "utf-8", null);
+
+            XmlElement rootNode = xmlDoc.CreateElement("VposRequest");
+            xmlDoc.InsertBefore(xmlDeclaration, xmlDoc.DocumentElement);
+            xmlDoc.AppendChild(rootNode);
+
+            //eklemek istediğiniz diğer elementleride bu şekilde ekleyebilirsiniz.
+            XmlElement merchantNode = xmlDoc.CreateElement("MerchantId"); //*** Üye işyeri numarası
+            XmlElement passwordNode = xmlDoc.CreateElement("Password"); //*** Üye işyeri şifresi
+            XmlElement terminalNode = xmlDoc.CreateElement("TerminalNo"); //*** İşlemin hangi terminal üzerinden gönderileceği bilgisi. ÜİY için tanımlanmış mevcut terminallerden herhangi birinin terminal numarası iletilmelidir. Ör: VB007000,...
+            XmlElement transactionTypeNode = xmlDoc.CreateElement("TransactionType");
+            XmlElement transactionIdNode = xmlDoc.CreateElement("TransactionId");
+            XmlElement currencyAmountNode = xmlDoc.CreateElement("CurrencyAmount");
+            XmlElement currencyCodeNode = xmlDoc.CreateElement("CurrencyCode");
+            XmlElement panNode = xmlDoc.CreateElement("Pan");
+            XmlElement cvvNode = xmlDoc.CreateElement("Cvv");
+            XmlElement expiryNode = xmlDoc.CreateElement("Expiry");
+            XmlElement ClientIpNode = xmlDoc.CreateElement("ClientIp");
+            XmlElement transactionDeviceSourceNode = xmlDoc.CreateElement("TransactionDeviceSource");
+
+            //yukarıda eklediğimiz node lar için değerleri ekliyoruz.
+            XmlText merchantText = xmlDoc.CreateTextNode("000000000019125"); //***
+            XmlText passwordtext = xmlDoc.CreateTextNode("burakarda1"); //***
+            XmlText terminalNoText = xmlDoc.CreateTextNode("Egitim17"); //***
+            XmlText transactionTypeText = xmlDoc.CreateTextNode("Sale");
+            XmlText transactionIdText = xmlDoc.CreateTextNode(Guid.NewGuid().ToString("N"));
+            XmlText currencyAmountText = xmlDoc.CreateTextNode("90.50"); //tutarı nokta ile gönderdiğinizden emin olunuz.
+            XmlText currencyCodeText = xmlDoc.CreateTextNode("949");
+            XmlText panText = xmlDoc.CreateTextNode("4543600299100712");
+            XmlText cvvText = xmlDoc.CreateTextNode("454");
+            XmlText expiryText = xmlDoc.CreateTextNode("201611");
+            XmlText ClientIpText = xmlDoc.CreateTextNode("190.20.13.12");
+            XmlText transactionDeviceSourceText = xmlDoc.CreateTextNode("0");
+
+            //nodeları root elementin altına ekliyoruz.
+            rootNode.AppendChild(merchantNode);
+            rootNode.AppendChild(passwordNode);
+            rootNode.AppendChild(terminalNode);
+            rootNode.AppendChild(transactionTypeNode);
+            rootNode.AppendChild(transactionIdNode);
+            rootNode.AppendChild(currencyAmountNode);
+            rootNode.AppendChild(currencyCodeNode);
+            rootNode.AppendChild(panNode);
+            rootNode.AppendChild(cvvNode);
+            rootNode.AppendChild(expiryNode);
+            rootNode.AppendChild(ClientIpNode);
+            rootNode.AppendChild(transactionDeviceSourceNode);
+
+            //nodelar için oluşturduğumuz textleri node lara ekliyoruz.
+            merchantNode.AppendChild(merchantText);
+            passwordNode.AppendChild(passwordtext);
+            terminalNode.AppendChild(terminalNoText);
+            transactionTypeNode.AppendChild(transactionTypeText);
+            transactionIdNode.AppendChild(transactionIdText);
+            currencyAmountNode.AppendChild(currencyAmountText);
+            currencyCodeNode.AppendChild(currencyCodeText);
+            panNode.AppendChild(panText);
+            cvvNode.AppendChild(cvvText);
+            expiryNode.AppendChild(expiryText);
+            ClientIpNode.AppendChild(ClientIpText);
+            transactionDeviceSourceNode.AppendChild(transactionDeviceSourceText);
+
+            returnValue.Result = xmlDoc.OuterXml;
+            returnValue.Success = true;
+            returnValue.Message = "Ödeme Xml text oluşturuldu.";
 
             return returnValue;
         }
