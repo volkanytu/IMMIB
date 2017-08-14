db5f11f4d84fbfc9d14e6a55fbc253a1902ae3c6
Volkan SERTER <volkan.serter@sahibinden.com>
2017-08-14 20:18:57 +0300

    object type codes added

---
+++ Library/SRC.Library.Entities/CrmEntities/ObjectTypeCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRC.Library.Entities.CrmEntities
+{
+    public enum ObjectTypeCode
+    {
+        ACCOUNT=1,
+        CONTACT=2
+    }
+}
